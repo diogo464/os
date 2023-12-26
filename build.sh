ac4a9f01a7823fda5ca1bf2d8cdbfc45c9edc15d
diogo464 <diogo464@protonmail.com>
2023-12-26 18:54:20 +0000

    init

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PREFIX="git.d464.sh/infra/os"
+TAG="latest"
+PUSH=${PUSH:-"0"}
+
+docker build -t "$PREFIX/base:$TAG" -f base.containerfile .
+docker build -t "$PREFIX/kube:$TAG" -f kube.containerfile .
+docker build -t "$PREFIX/nas:$TAG" -f nas.containerfile .
+
+if [ "$PUSH" -eq "1" ]; then
+	docker push "$PREFIX/base:$TAG"
+	docker push "$PREFIX/kube:$TAG"
+	docker push "$PREFIX/nas:$TAG"
+fi
