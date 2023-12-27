f558db8695cf38c63efca763ad0e22d698b26729
diogo464 <diogo464@protonmail.com>
2023-12-27 18:45:23 +0000

    removed invalid flag from k3s agent script

--- include/k3s-agent.sh
+++ include/k3s-agent.sh
@@ -3,7 +3,6 @@ mkdir -p /var/k3s
 k3s agent \
         --selinux \
         --data-dir /var/k3s \
-        --default-local-storage-path /var/volumes \
         --server $(cat /var/server) \
         --token-file /var/token || exit 1
 systemd-notify --ready
