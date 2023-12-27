c3cbe75f03e5b4c13502d51e653bbb33983dc5a3
diogo464 <diogo464@protonmail.com>
2023-12-27 13:14:27 +0000

    added builder image

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ PUSH=${PUSH:-"0"}
 docker build -t "$PREFIX/base:$TAG" -f base.containerfile .
 docker build -t "$PREFIX/kube:$TAG" -f kube.containerfile .
 docker build -t "$PREFIX/nas:$TAG" -f nas.containerfile .
+docker build -t "$PREFIX/builder:$TAG" -f builder.containerfile .
 
 if [ "$PUSH" -eq "1" ]; then
 	docker push "$PREFIX/base:$TAG"
 	docker push "$PREFIX/kube:$TAG"
 	docker push "$PREFIX/nas:$TAG"
+	docker push "$PREFIX/builder:$TAG"
 fi
