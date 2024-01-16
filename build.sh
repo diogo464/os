edaf46df12567fe2ab6480d39be2ffb586968e01
diogo464 <diogo464@protonmail.com>
2024-01-16 06:03:54 +0000

    refactored repo structure

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ PREFIX="git.d464.sh/infra/os"
 TAG="latest"
 PUSH=${PUSH:-"0"}
 
-docker build -t "$PREFIX/base:$TAG" -f base.containerfile .
-docker build -t "$PREFIX/kube:$TAG" -f kube.containerfile .
-docker build -t "$PREFIX/nas:$TAG" -f nas.containerfile .
-docker build -t "$PREFIX/builder:$TAG" -f builder.containerfile .
-docker build -t "$PREFIX/router:$TAG" -f router.containerfile .
+docker build -t "$PREFIX/base:$TAG" -f base/Containerfile base/
+docker build -t "$PREFIX/kube:$TAG" -f kube/Containerfile kube/
+docker build -t "$PREFIX/nas:$TAG" -f nas/Containerfile nas/
+docker build -t "$PREFIX/builder:$TAG" -f builder/Containerfile builder/
+docker build -t "$PREFIX/router:$TAG" -f router/Containerfile router/
 
 if [ "$PUSH" -eq "1" ]; then
 	docker push "$PREFIX/base:$TAG"
