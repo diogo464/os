d9b821f6fe3476f7b0c5245054ebbbd9f5608279
diogo464 <diogo464@protonmail.com>
2024-08-05 16:51:43 +0100

    nix test

---
+++ dotup.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+with pkgs;
+	rustPlatform.buildRustPackage rec {
+		pname = "dotup";
+		version = "0.3.0";
+		
+		src = fetchGit {
+			url = "https://git.d464.sh/code/dotup";
+			rev = "e62270b966a2cca1cffbb790cdc6a5130fd81c49";
+		};
+
+		RUSTC_BOOTSTRAP = true;
+		cargoSha256 = "sha256-L4GRRkMmm8x5ozPquJWrevaaKknCWNSKcyEEw8CqPc0=";
+	}
---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "A template that shows all standard flake outputs";
+
+  inputs.nixpkgsRegistry.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs, ... }: {
+        nixosConfigurations.nas = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [ ./configuration.nix ];
+        };
+  };
+}
