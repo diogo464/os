dfb7b0f968e8739b5c9a42466c1ea0ae5ab1023c
diogo464 <diogo464@d464.sh>
2025-01-28 21:49:26 +0000

    coreos fixed build for version 41

--- coreos/build.sh
+++ coreos/build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t git.d464.sh/infra/os/coreos:latest -f "$(dirname $0)/Containerfile" "$(dirname $0)/"
+docker build \
+	-t git.d464.sh/infra/os/coreos:latest \
+	-f "$(dirname $0)/Containerfile" \
+	--build-arg BUILDER_VERSION=$(curl -s "https://builds.coreos.fedoraproject.org/streams/stable.json" | jq -r '.architectures.x86_64.artifacts.metal.release' | cut -d '.' -f 1) \
+	"$(dirname $0)/"
 
 if [ "$PUSH" = "1" ]; then
 	docker push git.d464.sh/infra/os/coreos:latest
